0d277b5a2fe4b9768895a6b150bee23dfa1a2edf
姚旭 <lia77593@163.com>
2017-07-15 02:37:41 +0800

    Use SharpDX Impl Instead of CSCLI TODO(DirectSound)

---
+++ CSCore/COM/ObjectTracker.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace SharpDX.Diagnostics
+{
+    internal class ObjectTracker
+    {
+        internal static object Find(ComObject comObject)
+        {
+            throw new NotImplementedException();
+        }
+
+        internal static void UnTrack(ComObject comObject)
+        {
+            throw new NotImplementedException();
+        }
+
+        internal static void Track(ComObject comObject)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
